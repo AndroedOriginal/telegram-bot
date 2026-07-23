c651747c72694aa43ece0fb28aefb9f04e0a1650
AndroedOriginal <adrianmanukyan.k1@gmail.com>
2026-07-23 18:20:07 +0400

    Add admins_command to handle admin messages

--- handlers/admins.py
+++ handlers/admins.py
@@ -1,1 +1,13 @@
+from telegram import Update
+from telegram.ext import ContextTypes
 
+
+async def admins_command(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+
+    await update.message.reply_text(
+        "<tg-spoiler>Отчёт отправлен</tg-spoiler>",
+        parse_mode="HTML"
+    )
