0f50f77922becefc2073f7e474e1c7fd8b9f0e49
AndroedOriginal <adrianmanukyan.k1@gmail.com>
2026-07-23 18:44:38 +0400

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -2,20 +2,12 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 
-async def admins_report(
+async def admins_command(
     update: Update,
     context: ContextTypes.DEFAULT_TYPE
 ):
-
-    if update.message.from_user.is_bot:
-        return
-
-    text = update.message.text.lower()
-
-    if "@admins" in text:
-
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="<i>Отчёт отправлен</i>",
-            parse_mode="HTML"
+    if "@admins" in update.message.text:
+        await update.message.reply_text(
+            "*Отчёт отправлен*⁣⁣⁣⁣⁣⁣",
+            parse_mode="Markdown"
         )
