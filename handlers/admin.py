3fd0592af4a1e0601647c48d9871639dab41dbd6
AndroedOriginal <adrianmanukyan.k1@gmail.com>
2026-07-23 18:38:58 +0400

    Rename admins_command to admins_report and update logic

--- handlers/admin.py
+++ handlers/admin.py
@@ -2,13 +2,20 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 
-async def admins_command(
+async def admins_report(
     update: Update,
     context: ContextTypes.DEFAULT_TYPE
 ):
 
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="<i>Отчёт отправлен</i>",
-        parse_mode="HTML"
-    )
+    if update.message.from_user.is_bot:
+        return
+
+    text = update.message.text.lower()
+
+    if "@admins" in text:
+
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="<i>Отчёт отправлен</i>",
+            parse_mode="HTML"
+        )
