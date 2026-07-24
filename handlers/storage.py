26426538fa9352bca4b4325c895f07dfb1b78325
AndroedOriginal <adrianmanukyan.k1@gmail.com>
2026-07-24 12:12:20 +0400

    Refactor add_warn and remove_warn functions
    Refactor warning management to use loaded data instead of in-memory warnings.

--- handlers/storage.py
+++ handlers/storage.py
@@ -35,24 +35,34 @@ def get_warns(user_id):
 def add_warn(user_id):
     uid = str(user_id)
 
-    warnings[uid] = warnings.get(uid, 0) + 1
+    print("ДОБАВЛЯЕМ ВАРН:", uid)
 
-    save_warnings(warnings)
+    data = load_warnings()
 
-    return warnings[uid]
+    data[uid] = data.get(uid, 0) + 1
+
+    print("НОВЫЕ ДАННЫЕ:", data)
+
+    save_warnings(data)
+
+    print("ФАЙЛ СОХРАНЕН")
+
+    return data[uid]
 
 
 def remove_warn(user_id):
     uid = str(user_id)
 
-    if uid not in warnings:
+    data = load_warnings()
+
+    if uid not in data:
         return 0
 
-    warnings[uid] -= 1
+    data[uid] -= 1
 
-    if warnings[uid] <= 0:
-        del warnings[uid]
+    if data[uid] <= 0:
+        del data[uid]
 
-    save_warnings(warnings)
+    save_warnings(data)
 
-    return warnings.get(uid, 0)
+    return data.get(uid, 0)
