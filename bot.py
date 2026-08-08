194e19d74fb953233ce807c66203accb85f7d1d5
AndroedOriginal <adrianmanukyan.k1@gmail.com>
2026-08-08 15:19:16 +0400

    Refactor bot.py to use Application for bot commands

--- bot.py
+++ bot.py
@@ -1,42 +1,33 @@
 import os
 import json
-import asyncio
-
-from telegram import Bot, Update
-
-from handlers.schedule import setup_scheduler
 
 from telegram.ext import (
-Application,
-MessageHandler,
-CommandHandler,
-CallbackQueryHandler,
-filters
+    Application,
+    MessageHandler,
+    CommandHandler,
+    CallbackQueryHandler,
+    filters
 )
-
-from handlers.admin import admins_command
-
-from handlers.welcome import welcome_new_member
-
-from handlers.warn import warn_command, cancel_warn
-
-from handlers.unmute import unmute_command
-
-TOKEN = os.getenv("TOKEN")
-if not TOKEN:
-    raise Exception("TOKEN не найден в Variables")
 
 from telegram.request import HTTPXRequest
 
+from handlers.schedule import setup_scheduler
+from handlers.admin import admins_command
+from handlers.welcome import welcome_new_member
+from handlers.warn import warn_command, cancel_warn
+from handlers.unmute import unmute_command
 from handlers.rules import rules_command
-
 from handlers.msg import msg_command
-
 from handlers.ban import ban_command, unban_callback
-
 from handlers.mute import mute_command
-
 from handlers.owner import owner_command
+
+
+TOKEN = os.getenv("TOKEN")
+
+if not TOKEN:
+    raise Exception("TOKEN не найден в Variables")
+
 
 request = HTTPXRequest(
     connect_timeout=30,
@@ -45,6 +36,7 @@ request = HTTPXRequest(
     pool_timeout=30
 )
 
+
 application = (
     Application.builder()
     .token(TOKEN)
@@ -52,7 +44,9 @@ application = (
     .build()
 )
 
+
 print("TOKEN найден:", TOKEN is not None)
+
 
 with open("config.json", "r", encoding="utf-8") as file:
     config = json.load(file)
@@ -60,12 +54,12 @@ with open("config.json", "r", encoding="utf-8") as file:
 
 CHAT_ID = config["chat_id"]
 
-bot = Bot(TOKEN)
-
 
 async def send_message(text):
+
     try:
-        await bot.send_message(
+
+        await application.bot.send_message(
             chat_id=CHAT_ID,
             text=text,
             parse_mode="HTML"
@@ -74,117 +68,113 @@ async def send_message(text):
         print("Сообщение отправлено:", text)
 
     except Exception as e:
-        print("Ошибка отправки:", repr(e))
 
-
-async def main():
-
-    print("УНИКАЛЬНЫЙ ЗАПУСК БОТА")
-
-
-    scheduler = setup_scheduler(
-        config,
-        send_message
-    )
-
-
-    scheduler.start()
-
-
-    application.add_handler(
-        CommandHandler(
-            "warn",
-            warn_command
+        print(
+            "Ошибка отправки:",
+            repr(e)
         )
-    )
-    
-    
-    application.add_handler(
-        CallbackQueryHandler(
-            cancel_warn,
-            pattern="^cancel_warn_"
-        )
-    )
-    
-    
-    application.add_handler(
-        MessageHandler(
-            filters.StatusUpdate.NEW_CHAT_MEMBERS,
-            welcome_new_member
-        )
-    )
-    
-    
-    application.add_handler(
-        MessageHandler(
-            filters.TEXT & filters.Regex("@admins"),
-            admins_command
-        )
-    )
-
-    application.add_handler(
-        CommandHandler(
-            "unmute",
-            unmute_command
-        )
-    )
-
-    application.add_handler(
-        CommandHandler(
-            "rules",
-            rules_command
-        )
-    )
-
-    application.add_handler(
-        CommandHandler(
-            "send",
-            msg_command
-        )
-    )
-
-    application.add_handler(
-        CommandHandler(
-            "ban",
-            ban_command
-        )
-    )
-    
-    
-    application.add_handler(
-        CallbackQueryHandler(
-            unban_callback,
-            pattern="unban"
-        )
-    )
-
-    application.add_handler(
-        CommandHandler(
-            "mute",
-            mute_command
-        )
-    )
-
-    application.add_handler(
-        CommandHandler(
-            "owner",
-            owner_command
-        )
-    )
-    
-    await application.initialize()
-    await application.start()
-    await application.updater.start_polling()
-
-    print("Бот запущен!")
-
-    try:
-        await asyncio.Event().wait()
-    
-    finally:
-        await application.updater.stop()
-        await application.stop()
-        await application.shutdown()
 
 
-asyncio.run(main())
+application.add_handler(
+    CommandHandler(
+        "warn",
+        warn_command
+    )
+)
+
+
+application.add_handler(
+    CallbackQueryHandler(
+        cancel_warn,
+        pattern=r"^cancel_warn_"
+    )
+)
+
+
+application.add_handler(
+    MessageHandler(
+        filters.StatusUpdate.NEW_CHAT_MEMBERS,
+        welcome_new_member
+    )
+)
+
+
+application.add_handler(
+    MessageHandler(
+        filters.TEXT & filters.Regex("@admins"),
+        admins_command
+    )
+)
+
+
+application.add_handler(
+    CommandHandler(
+        "unmute",
+        unmute_command
+    )
+)
+
+
+application.add_handler(
+    CommandHandler(
+        "rules",
+        rules_command
+    )
+)
+
+
+application.add_handler(
+    CommandHandler(
+        "send",
+        msg_command
+    )
+)
+
+
+application.add_handler(
+    CommandHandler(
+        "ban",
+        ban_command
+    )
+)
+
+
+application.add_handler(
+    CallbackQueryHandler(
+        unban_callback,
+        pattern=r"^unban"
+    )
+)
+
+
+application.add_handler(
+    CommandHandler(
+        "mute",
+        mute_command
+    )
+)
+
+
+application.add_handler(
+    CommandHandler(
+        "owner",
+        owner_command
+    )
+)
+
+
+scheduler = setup_scheduler(
+    config,
+    send_message
+)
+
+scheduler.start()
+
+print("Планировщик запущен")
+
+
+print("УНИКАЛЬНЫЙ ЗАПУСК БОТА")
+
+application.run_polling(
+    drop_pending_updates=False
+)
